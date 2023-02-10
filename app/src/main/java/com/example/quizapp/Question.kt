5c346f729bfac3d7e15079bc29a43f2b17004b93
Eric <eric.degomar@gmail.com>
2023-02-10 16:48:41 -0300

    lista com perguntas, data class e object de lista criados

--- app/src/main/java/com/example/quizapp/Question.kt
+++ app/src/main/java/com/example/quizapp/Question.kt
@@ -1,3 +1,13 @@
 package com.example.quizapp
 
-data class Question()
+data class Question(
+    var id: Int,
+    var image: Int,
+    var question: String,
+    var option1: String,
+    var option2: String,
+    var option3: String,
+    var option4: String,
+    var correctAnswer: Int
+
+)
--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -2,6 +2,7 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.quizapp.databinding.ActivityQuizQuestionsBinding
 
@@ -9,10 +10,15 @@ class QuizQuestionsActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityQuizQuestionsBinding
 
+    lateinit var questionList: ArrayList<Question>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_quiz_questions)
 
+
+        questionList = Constants.getQuestions()
+        Log.i("lista","lista do tamanho ${questionList.size}")
 
     }
 }
